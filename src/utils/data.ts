4f4a55fb1617fa98b8b8396755fd242e3c692a49
shahriar <amivaloasi>
2023-10-03 14:22:51 +0600

    add project 5

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -46,7 +46,7 @@ export const portfolios = [
       "<p>My React SaaS applications project is a modern and responsive web application built using Next JS, Recoil, Tailwind CSS, and SWR. The project showcases my expertise in building scalable and reliable web applications with an emphasis on user experience and performance.</p><p>As a Frontend Developer, my role in this project was to design and develop the user interface and implement the necessary functionality for a SaaS application. The project features a modern and responsive user interface with seamless navigation, intuitive user flows, and fast loading times.</p><p>The React SaaS applications project is a great example of my proficiency in modern web development technologies and my ability to deliver high-quality work in a timely and efficient manner. ",
   },
   {
-    id: "3",
+    id: "4",
     title: "MyFarmBD - Empowering Finance and Food Security in Bangladesh",
     about: "",
     tech: ["Next JS", "Redux", "Scss", "SWR"],
@@ -66,5 +66,24 @@ export const portfolios = [
     live: "https://www.myfarmbd.com/",
     description:
       "<p>MyFarmBD is a platform that enables people to contribute to achieving food security for Bangladesh while also growing their funds. With a simple, safe, and sustainable investment platform, investors can support real farmers in growing food for the Bangladeshi population. The platform offers a spectrum of investable farms, allowing micro-investors and high network individuals alike to invest in the cause of food security for Bangladesh and achieve financial freedom. </p><p>As a Frontend Developer on this project, I used Next.js and SWR for API calling, Redux for state management, and integrated with the Laravel backend. MyFarmBD's user interface is designed to be user-friendly, engaging, and responsive, with a focus on delivering an intuitive and streamlined user experience. </p>",
+  },
+  {
+    id: "5",
+    title: "A simple animated Card",
+    about: "",
+    tech: ["HTML", "CSS", "Javascript"],
+    images: ["/images/project5/1.png"],
+    role: "Frontend Developer",
+    github: "https://github.com/Shahriar-Shakil/animated-bg-card",
+    live: "https://animated-bg-card.netlify.app/",
+    description: `<h1>Randomly Animated Boxes</h1>
+    <p>The "Randomly Animated Boxes" project is a dynamic web application that generates a visually engaging display of colored boxes scattered randomly across the height and width of the viewport. The project uses HTML, CSS, and JavaScript to create a canvas of 100 boxes, where 20% of them belong to a special class. These boxes are animated with varying properties:</p>
+    <ul>
+        <li><strong>Color:</strong> The boxes change colors randomly at their own speed.</li>
+        <li><strong>Position:</strong> The boxes are positioned randomly across the viewport.</li>
+        <li><strong>Animation Delay:</strong> Each "animated-box" has a random delay before starting its animation, creating a staggered effect.</li>
+        <li><strong>Animation Duration:</strong> Each "animated-box" has a random animation duration, resulting in boxes moving at different speeds.</li>
+    </ul>
+    <p>The combination of these features results in a visually dynamic and interactive web page that demonstrates the power of web technologies in creating engaging user experiences.</p>`,
   },
 ];
